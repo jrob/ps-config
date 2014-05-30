a3426bc2ddc944fc325543de27568828b0a1be60
Jeremy Roberts <jeremy@robertsisland.com>
2014-05-30 11:14:55 -0400

    Adding gvim helper.

--- Profile.ps1
+++ Profile.ps1
@@ -26,6 +26,8 @@ Import-Module "$scripts\powershell\posh-git"
 #Set-Alias bc "C:\Program Files (x86)\Beyond Compare 3\BComp.com"
 Set-Alias di ls
 
+Function ve { gvim.exe --remote-tab-silent $args }
+
 Set-Alias mr ($scripts + "\PowerShell\MassRename.ps1")
 Set-Alias which ($scripts + "\PowerShell\Which.ps1")
 
