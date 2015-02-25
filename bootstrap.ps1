cadead1f586f28ffdca4e7c9737d708300c468c1
Jeremy Roberts <jeremy@robertsisland.com>
2015-02-25 13:27:17 -0500

    Adding bootstrap script.

---
+++ bootstrap.ps1
@@ -0,0 +1,5 @@
+﻿Set-ExecutionPolicy RemoteSigned -force
+iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install -y git
+& "C:\Program Files (x86)\Git\cmd\git.exe" clone https://github.com/jrob/ps-config.git "$home\Scripts\Powershell"
+exit
