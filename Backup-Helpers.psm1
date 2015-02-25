17d8bed33be0c2bd7a873af3c0f4518d8f112616
Jeremy Roberts <jeremy@robertsisland.com>
2015-02-25 13:23:53 -0500

    Adding backup helper script.

---
+++ Backup-Helpers.psm1
@@ -0,0 +1,39 @@
+﻿function Run-Backups($locations)
+{
+    foreach ($i in $locations)
+    {
+        $src = $i.src
+        $dst = $i.dst
+        robocopy "$src" "$dst" /MIR /ZB /V
+    }
+}
+
+function Restore-Backups($locations)
+{
+    foreach ($i in $locations)
+    {
+        $src = $i.src
+        $dst = $i.dst
+        robocopy "$dst" "$src" /E
+    }
+}
+
+function Schedule-Backup-Tasks($locations, $logfilepath)
+{
+    $actions = @()
+
+    $logopt = "/log:$logfilepath"
+    foreach ($i in $locations)
+    {
+        $src = $i.src
+        $dst = $i.dst
+        #Write-Host """$src"" ""$dst"" /MIR /ZB /V"
+        $newTask = New-ScheduledTaskAction -Execute 'C:\Windows\System32\Robocopy.exe' -Argument """$src"" ""$dst"" /MIR /ZB /V $logopt"
+        $actions += $newTask
+        $logopt = "/log+:$logfilepath"
+    }
+
+    $Stt = New-ScheduledTaskTrigger -Daily -At 3am
+
+    Register-ScheduledTask -Action $actions -TaskName "Run-Robocopy" -Description "Run robocopy batch jobs." -Trigger $Stt -RunLevel Highest
+}
