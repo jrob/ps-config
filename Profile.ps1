5c89b0d37c9127a8c0edb03364516adb878f4cd5
Jeremy Roberts <jeremy@robertsisland.com>
2016-09-29 17:09:48 -0400

    Removing commented lines.

--- Profile.ps1
+++ Profile.ps1
@@ -9,15 +9,6 @@ $env:PATHEXT += ";.pl"
 
 Import-Module Posh-Git
 
-#$e=Get-Date; ($e - $s).TotalSeconds
-
-#. "$scripts\powershell\ssh-agent-utils.ps1"
-
-#Set-Alias pn "C:\Program Files (x86)\Programmer's Notepad\pn.exe"
-#Set-Alias sz "C:\Program Files\7-Zip\7z.exe"
-#Set-Alias vim "C:\Program Files (x86)\Vim\vim73\vim.exe"
-#Set-Alias sub "C:\Program Files\Sublime Text 2\sublime_text.exe"
-#Set-Alias bc "C:\Program Files (x86)\Beyond Compare 3\BComp.com"
 Set-Alias di ls
 
 Function vi { & "C:\Program Files (x86)\vim\vim74\gvim.exe" --remote-tab-silent $args }
@@ -28,38 +19,6 @@ Set-Alias which ($scripts + "\PowerShell\Which.ps1")
 Set-Alias formd ($scripts + "\Python\formd\src\formd.py")
 
 function mako { python ($scripts + "\Python\mako-render.py") $args }
-
-# To force git to display colors
-#$env:TERM = 'cygwin'
-#$env:LESS = 'FRSX'
-
-#$a = (Get-Host).UI.RawUI
-#$a.BackgroundColor = "rgb(50,50,50)"
-
-# http://www.nivot.org/nivot2/post/2008/06/27/WorkaroundToForcePowerShellRedirectionOperatorToUseASCIIEncoding.aspx
-#function out-file($FilePath, $Encoding, [switch]$Append) {
-#    $input | microsoft.powershell.utility\out-file $filepath -encoding ascii -append:$append
-#}
-
-# http://blog.clintarmstrong.net/2011/05/powershell-history-persistence.html
-#$MaximumHistoryCount = 31KB
-
-#function bye
-#{
-#    Get-History -Count 31KB | Export-CSV ~\PowerShell-history.csv
-#    exit
-#}
-
-#if (Test-Path ~\PowerShell-History.csv)
-#{
-#    Import-CSV ~\PowerShell-History.csv | Add-History
-#    Get-History -Count 31KB | Export-CSV ~\PowerShell-history.csv
-#}
-
-# http://blog.joonro.net/en/2013/12/20/persistent_history_and_history_search_with_arrow_keys_in_powershell.html
-# Persistent history with duplicate removal
-#
-# http://orsontyrell.blogspot.ca/2013/11/true-powershell-command-history.html
 
 $MaximumHistoryCount = 31KB
 $ImportedHistoryCount = 0
@@ -114,15 +73,6 @@ cd ~
 # Shamelessly modified from http://winterdom.com/2008/08/mypowershellprompt
 $Global:skip = $true
 function prompt {
-    #$hid = $myinvocation.historyID
-    #if ($hid -gt 1 -and -not $skip) {
-    #    Get-History ($myinvocation.historyID -1 ) |
-    #        ConvertTo-Csv |
-    #        Select -last 1 |
-    #        Add-Content -Encoding UTF8 ~\PowerShell-History.csv
-    #}
-    #$Global:skip = $false
-
     # our theme
     $cdelim = [ConsoleColor]::DarkCyan
     $ctime = [ConsoleColor]::Blue
