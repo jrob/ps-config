c4f137f127cc0067d69238ab909672eb87d60150
Jeremy Roberts <jeremy@robertsisland.com>
2017-08-18 13:10:16 -0400

    Remove vs proxy settings.

--- Proxy-Helpers.psm1
+++ Proxy-Helpers.psm1
@@ -1,30 +1,3 @@
-$VsConfigFile = "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe.config"
-
-function Get-VsConfigNoProxy
-{
-    return @"
-        <system.net>
-            <settings>
-                <ipv6 enabled="true"/>
-            </settings>
-        </system.net>
-"@
-}
-
-function Get-VsConfigWithProxy($proxy)
-{
-    return @"
-        <system.net>
-            <settings>
-                <ipv6 enabled="true"/>
-            </settings>
-            <defaultProxy useDefaultCredentials="true" enabled="true">
-                <proxy bypassonlocal="true" proxyaddress="$proxy" />
-            </defaultProxy>
-        </system.net>
-"@
-}
-
 function Set-NoProxy($noProxy)
 {
     [Environment]::SetEnvironmentVariable("no_proxy", $noProxy, "User")
@@ -40,9 +13,6 @@ function Set-Proxy($proxy)
     $http_proxy = $proxy
     $https_proxy = $proxy
     choco config set proxy $proxy
-    $VsConfigWithProxy = Get-VsConfigWithProxy $proxy
-    $VsConfigNoProxy = Get-VsConfigNoProxy
-    Replace-Chunk-In-File $VsConfigFile $VsConfigNoProxy $VsConfigWithProxy
 }
 
 function Clear-Proxy
@@ -55,16 +25,6 @@ function Clear-Proxy
     $http_proxy = $null
     $https_proxy = $null
     choco config unset proxy
-    $VsConfigWithProxy = Get-VsConfigWithProxy $oldProxy
-    $VsConfigNoProxy = Get-VsConfigNoProxy
-    Replace-Chunk-In-File  $VsConfigFile $VsConfigWithProxy $VsConfigNoProxy
-}
-
-function Replace-Chunk-In-File($filename, $before, $after)
-{
-    $fileContent =  Get-Content $filename -Raw
-    $newFileContent = $fileContent -replace $before, $after
-    Set-Content -Path $filename -Value $newFileContent
 }
 
 export-modulemember -function Set-Proxy
