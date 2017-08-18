36e04881e1373e560ae6f915b06169d304dd6704
Jeremy Roberts <jeremy@robertsisland.com>
2017-08-18 13:26:32 -0400

    Adding ie proxy support.

--- Proxy-Helpers.psm1
+++ Proxy-Helpers.psm1
@@ -6,16 +6,62 @@ function Set-NoProxy($noProxy)
 
 function Set-Proxy {
     [CmdletBinding()] param(
-        [String[]]$Proxy
+        [String[]]$Proxy,
+
+        [AllowEmptyString()]
+        [String[]]$Acs
     )            
 
     Clear-Proxy
     Set-ProxyEnvironmentVariable $Proxy
+    Set-IeProxy $Proxy $Acs
 }
 
 function Clear-Proxy
 {
     Set-ProxyEnvironmentVariable $null
+    Clear-IeProxy
+}
+
+function Set-IeProxy {
+    [CmdletBinding()] param(
+        [String[]]$Proxy,
+
+        [AllowEmptyString()]
+        [String[]]$acs
+    ) 
+    $null = New-PSDrive -PSProvider Registry -Name HKU -Root HKEY_USERS
+    $sid = Get-CurrentUserSID
+    $reg = "HKU:\$sid\Software\Microsoft\Windows\CurrentVersion\Internet Settings"
+
+    Set-ItemProperty -Path $reg -Name ProxyServer -Value $proxy
+    Set-ItemProperty -Path $reg -Name ProxyEnable -Value 1
+    if($acs) {
+        Set-ItemProperty -Path $reg -Name AutoConfigURL -Value $acs
+    }
+    else {
+        Remove-ItemProperty -Path $reg -Name AutoConfigURL -ErrorAction SilentlyContinue
+    }
+    Remove-PSDrive -Name HKU
+
+    $ie = new-object -ComObject internetexplorer.application
+    $ie.quit()
+}
+
+function Clear-IeProxy {
+    [CmdletBinding()] param()            
+
+    $null = New-PSDrive -PSProvider Registry -Name HKU -Root HKEY_USERS
+    $sid = Get-CurrentUserSID
+    $reg = "HKU:\$sid\Software\Microsoft\Windows\CurrentVersion\Internet Settings"
+
+    Remove-ItemProperty -Path $reg -Name ProxyServer -ErrorAction SilentlyContinue
+    Set-ItemProperty -Path $reg -Name ProxyEnable -Value 0
+    Remove-ItemProperty -Path $reg -Name AutoConfigURL -ErrorAction SilentlyContinue
+    Remove-PSDrive -Name HKU
+
+    $ie = new-object -ComObject internetexplorer.application
+    $ie.quit()
 }
 
 function Set-ProxyEnvironmentVariable($proxy)
@@ -26,6 +72,14 @@ function Set-ProxyEnvironmentVariable($proxy)
     [Environment]::SetEnvironmentVariable("https_proxy", $proxy, "Process")
     $http_proxy = $proxy
     $https_proxy = $proxy
+}
+
+function Get-CurrentUserSID {            
+    [CmdletBinding()] param()            
+
+    $objUser = New-Object System.Security.Principal.NTAccount( $env:USERDOMAIN, $env:USERNAME)
+    $strSID = $objUser.Translate([System.Security.Principal.SecurityIdentifier])
+    return $strSID.Value
 }
 
 export-modulemember -function Set-Proxy
