e990c002af4c953522ed387bc2ca0ffbfe44c672
Jeremy Roberts <jeremy@robertsisland.com>
2017-08-18 13:16:42 -0400

    Set proxy for machine vs user.

--- Proxy-Helpers.psm1
+++ Proxy-Helpers.psm1
@@ -1,13 +1,13 @@
 function Set-NoProxy($noProxy)
 {
-    [Environment]::SetEnvironmentVariable("no_proxy", $noProxy, "User")
+    [Environment]::SetEnvironmentVariable("no_proxy", $noProxy, "Machine")
     [Environment]::SetEnvironmentVariable("no_proxy", $noProxy, "Process")
 }
 
 function Set-Proxy($proxy)
 {
-    [Environment]::SetEnvironmentVariable("http_proxy", $proxy, "User")
-    [Environment]::SetEnvironmentVariable("https_proxy", $proxy, "User")
+    [Environment]::SetEnvironmentVariable("http_proxy", $proxy, "Machine")
+    [Environment]::SetEnvironmentVariable("https_proxy", $proxy, "Machine")
     [Environment]::SetEnvironmentVariable("http_proxy", $proxy, "Process")
     [Environment]::SetEnvironmentVariable("https_proxy", $proxy, "Process")
     $http_proxy = $proxy
@@ -17,8 +17,8 @@ function Set-Proxy($proxy)
 function Clear-Proxy
 {
     $oldProxy = $env:https_proxy
-    [Environment]::SetEnvironmentVariable("http_proxy", $null, "User")
-    [Environment]::SetEnvironmentVariable("https_proxy", $null, "User")
+    [Environment]::SetEnvironmentVariable("http_proxy", $null, "Machine")
+    [Environment]::SetEnvironmentVariable("https_proxy", $null, "Machine")
     [Environment]::SetEnvironmentVariable("http_proxy", $null, "Process")
     [Environment]::SetEnvironmentVariable("https_proxy", $null, "Process")
     $http_proxy = $null
