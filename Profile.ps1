bdfcafb313859b196bafa7e49e0925db6cd320de
Jeremy Roberts <jeremy@robertsisland.com>
2020-03-17 15:30:27 -0400

    Message if chocolaty profile is not available.

--- Profile.ps1
+++ Profile.ps1
@@ -5,6 +5,9 @@ $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
+else {
+    Write-Host "ChocolateyProfile does not exist"
+}
 
 if (Get-Module -ListAvailable -Name PSReadLine) {
     Import-Module PSReadLine
