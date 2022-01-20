bf640b1aac4d850693fb9f3257bc04a1aa0049d2
Jeremy Roberts <jeremy@robertsisland.com>
2022-01-20 14:18:56 -0500

    Profile update.

--- Profile.ps1
+++ Profile.ps1
@@ -1,3 +1,5 @@
+Write-Host "jrob profile"
+
 $psScripts = "$home\dev\ps-config"
 
 # Chocolatey profile
@@ -15,7 +17,7 @@ if (Get-Module -ListAvailable -Name PSReadLine) {
     Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
     Set-PSReadlineOption -HistoryNoDuplicates
     Set-PSReadlineOption -MaximumHistoryCount 16384
-} 
+}
 else {
     Write-Host "PSReadLine module does not exist"
 }
@@ -23,7 +25,7 @@ else {
 if (Get-Module -ListAvailable -Name Posh-Git) {
     Import-Module Posh-Git
     $poshgit = $true
-} 
+}
 else {
     Write-Host "Posh-Git module does not exist"
 }
@@ -43,6 +45,8 @@ Function vi { & "C:\Program Files (x86)\vim\vim74\gvim.exe" --remote-tab-silent 
 Set-Alias mr "$psScripts\MassRename.ps1"
 Set-Alias which "$psScripts\Which.ps1"
 
+Set-PoshPrompt -Theme paradox
+
 # Set colors
 
 # git clone https://github.com/lukesampson/concfg
@@ -51,37 +55,37 @@ Set-Alias which "$psScripts\Which.ps1"
 # cd ~
 
 # Shamelessly modified from http://www.winterdom.com/powershell/2008/08/13/mypowershellprompt.html
-$Global:skip = $true
-function prompt {
-    # our theme
-    $cdelim = [ConsoleColor]::DarkCyan
-    $ctime = [ConsoleColor]::Blue
-    $chost = [ConsoleColor]::Green
-    $cloc = [ConsoleColor]::Cyan
-
-    #write-host -NoNewline -ForegroundColor $cloc "$([char]0x0A7) "
-    Write-Host -NoNewline -ForegroundColor $ctime (Get-Date -Format "yyyy-MM-dd HHmm ")
-    Write-Host -NoNewline -ForegroundColor $chost "$env:username@"
-    Write-Host -NoNewline -ForegroundColor $chost ([net.dns]::GetHostName())
-    #write-host -NoNewline -ForegroundColor $cdelim ' {'
-    Write-Host -NoNewline -ForegroundColor $cdelim ' '
-    Write-Host -NoNewline -ForegroundColor $cloc (shorten-path (pwd).Path)
-    #write-host -f $cdelim '}'
-    if ($poshgit) { Write-VcsStatus }
-    Write-Host -ForegroundColor $cdelim ''
-    Write-Host -NoNewline '$'
-    return ' '
-}
-
-function shorten-path([string] $path) {
-   # For Win 7
-   $loc = $path.Replace($HOME, '~')
-   # For XP
-   $loc = $loc.Replace($env:home, '~')
-   # remove prefix for UNC paths
-   $loc = $loc -replace '^[^:]+::', ''
-   # make path shorter like tabs in Vim,
-   # handle paths starting with \\ and . correctly
-   # $loc = $loc -replace '\\(\.?)([^\\])[^\\]*(?=\\)','\$1$2'
-   return $loc
-}
+# $Global:skip = $true
+# function prompt {
+#     # our theme
+#     $cdelim = [ConsoleColor]::DarkCyan
+#     $ctime = [ConsoleColor]::Blue
+#     $chost = [ConsoleColor]::Green
+#     $cloc = [ConsoleColor]::Cyan
+#
+#     #write-host -NoNewline -ForegroundColor $cloc "$([char]0x0A7) "
+#     Write-Host -NoNewline -ForegroundColor $ctime (Get-Date -Format "yyyy-MM-dd HHmm ")
+#     Write-Host -NoNewline -ForegroundColor $chost "$env:username@"
+#     Write-Host -NoNewline -ForegroundColor $chost ([net.dns]::GetHostName())
+#     #write-host -NoNewline -ForegroundColor $cdelim ' {'
+#     Write-Host -NoNewline -ForegroundColor $cdelim ' '
+#     Write-Host -NoNewline -ForegroundColor $cloc (shorten-path (pwd).Path)
+#     #write-host -f $cdelim '}'
+#     if ($poshgit) { Write-VcsStatus }
+#     Write-Host -ForegroundColor $cdelim ''
+#     Write-Host -NoNewline '$'
+#     return ' '
+# }
+#
+# function shorten-path([string] $path) {
+#    # For Win 7
+#    $loc = $path.Replace($HOME, '~')
+#    # For XP
+#    $loc = $loc.Replace($env:home, '~')
+#    # remove prefix for UNC paths
+#    $loc = $loc -replace '^[^:]+::', ''
+#    # make path shorter like tabs in Vim,
+#    # handle paths starting with \\ and . correctly
+#    # $loc = $loc -replace '\\(\.?)([^\\])[^\\]*(?=\\)','\$1$2'
+#    return $loc
+# }
